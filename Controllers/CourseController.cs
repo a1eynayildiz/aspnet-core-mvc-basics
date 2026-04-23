772fcc7aefa0a819e288cf831d875c9fe9d1e282
Aleyna Yıldız <aleynayildizc@gmail.com>
2026-04-23 13:25:18 +0300

    implement course listing page: create List action with sample data and update view to render course list

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -13,32 +13,7 @@ public class CourseController : Controller
 
 
     //localhost:3000/course/list
-    public ActionResult List()
-    {
-        string kursAdi1 = "Javascript Kursu ";
-        string kursAdi2 = "React Kursu";
-
-        string kursAciklama1 = "JavaScript Kurs Açıklaması";
-        string kursAciklama2 = "React Kurs Açıklaması";
-
-        string[] kursResimleri = ["1.jpg","2.jpg"];
-
-
-        string[] kursAdlari = ["Javascript Kursu","React Kursu"];
-        string[] kursAciklamalari = ["JavaScript Kurs Açıklaması","React Kurs Açıklaması"] ;
-
-        ViewData["kursAdi1"] = kursAdlari[0];//javascript
-        ViewData["kursAdi2"] = kursAdlari[1];//react
-
-        ViewData["kursAciklama1"] = kursAciklamalari[0];//javascript
-        ViewData["kursAciklama2"] = kursAciklamalari[1];//react
-
-        ViewData["kursResim1"] = kursResimleri[0];//javascript
-        ViewData["kursResim2"] = kursResimleri[1];//react
-
-        return View();
-    }
-
+    
     //localhost:3000/course/details
     public ActionResult Details()
     {
@@ -46,7 +21,27 @@ public class CourseController : Controller
         kurs1.Title ="Django kursu";
         kurs1.Image = "1.jpg";
         
-        return View();
+        return View(kurs1);
        
     }
+
+    public ActionResult List()
+    {
+        Course kurs1 = new Course();
+        kurs1.Title = "Javascript Kursu ";
+        kurs1.Image ="1.jpg";
+
+        Course kurs2 = new Course();
+        kurs2.Title = "React Kursu ";
+        kurs2.Image ="2.jpg";
+
+
+        Course[] kurslar = [kurs1,kurs2];
+
+       
+        return View(kurslar);
+    }
+
+
+    
 }
