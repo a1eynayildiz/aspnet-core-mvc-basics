3f1fee6df8cf848f11dac25ea398dd622792dadb
Aleyna Yıldız <aleynayildizc@gmail.com>
2026-04-23 14:48:05 +0300

    implement product listing page with bootstrap card layout and sample data

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -12,7 +12,16 @@ public class ProductsController : Controller
 
     public ActionResult List()
     {
-        return View();
+        List<Product> urunler = new List<Product>
+        {
+            new Product{urunBaslik = "Iphone 15", urunFiyat =47249, urunResim="i1.png", urunAciklama = "iPhone 15’in ekranı yuvarlatılmış köşelere sahiptir. Telefonun kıvrımlı, zarif tasarımını takip eden bu köşeler standart bir dikdörtgenin sınırları içinde yer alır. Standart bir dikdörtgen olarak ölçüldüğünde ekran diyagonal olarak 6.12 inçtir (görüntülenebilen gerçek alan daha azdır",urunSatistami=true},
+
+            new Product{urunBaslik = "Iphone 16", urunFiyat =57999, urunResim="i2.png", urunAciklama = "iPhone 16’nın ekranı yuvarlatılmış köşelere sahiptir. Telefonun kıvrımlı, zarif tasarımını takip eden bu köşeler standart bir dikdörtgenin sınırları içinde yer alır. Standart bir dikdörtgen olarak ölçüldüğünde ekran diyagonal olarak 6.12 inçtir (görüntülenebilen gerçek alan daha azdır",urunSatistami=true},
+
+            new Product{urunBaslik = "Iphone 17", urunFiyat =73999, urunResim="i3.png", urunAciklama = "iPhone 17’nın ekranı yuvarlatılmış köşelere sahiptir. Telefonun kıvrımlı, zarif tasarımını takip eden bu köşeler standart bir dikdörtgenin sınırları içinde yer alır. Standart bir dikdörtgen olarak ölçüldüğünde ekran diyagonal olarak 6.12 inçtir (görüntülenebilen gerçek alan daha azdır",urunSatistami=false}
+
+        };
+        return View(urunler);
     }
 
     public ActionResult Details()
